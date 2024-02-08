91696b0280c250b351f670afd9f9acbc31a6bb8a
Daniela Codes <daniela.antonio.430@gmail.com>
2024-02-08 00:47:21 -0500

    Final_v1 1 for Task 4
    Updated index.js file
    
    included Morgan middleware
    
    created an error-handling

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
 const express = require('express');
+const  morgan = require('morgan');
+const bodyParser = require('body-parser');
+const  methodOverride = require('method-override');
+
 const app = express();
 
-let myLogger = (req, res, next) => {
+let myLogger = (req,res,next) => {
   console.log(req.url);
   next();
 };
 
-app.get('/movies', (req, res) => {
-  res.send('My Top 10 Movies : ');
+app.use(myLogger);
+app.use(morgan('common'));
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
 });
 
+app.get('/movies', (req, res, next) => {
+   res.send('My Top 10 Movies: ');
+});
+  
 app.get('/', (req, res) => {
   res.send('This is the endpoint! ');
 });
@@ -17,3 +37,5 @@ app.get('/', (req, res) => {
 app.listen(8080, () => {
   console.log('Your app is listening on port 8080.');
 });
+
+
