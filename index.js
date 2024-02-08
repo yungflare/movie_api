562fc4a314765dc3a4ef2102d533f6cdf9ce1387
Daniela Codes <daniela.antonio.430@gmail.com>
2024-02-07 23:16:40 -0500

    Update - Task 4
    Deleted practice code
    
    Replaced :
    
    ' / ' in line 25 with '/movies' which becomes "top 10"
    
    '/secreturl' with '/' as an endpoint

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-// const express = require('express');
-// const app = express();
-
-// const http = require('http');
-
-// http.createServer((request, response) => {
-//   response.writeHead(200, {'Content-Type': 'text/plain'});
-//   response.end('Welcome to my book club!\n');
-// }).listen(8080);
-
-// console.log('My first Node test server is running on Port 8080.');
-
-
-// 
 const express = require('express');
 const app = express();
 
@@ -20,14 +6,12 @@ let myLogger = (req, res, next) => {
   next();
 };
 
-app.use(myLogger);
-
-app.get('/', (req, res) => {
-  res.send('Welcome to my app!');
+app.get('/movies', (req, res) => {
+  res.send('My Top 10 Movies : ');
 });
 
-app.get('/secreturl', (req, res) => {
-  res.send('This is a secret url with super top-secret content.');
+app.get('/', (req, res) => {
+  res.send('This is the endpoint! ');
 });
 
 app.listen(8080, () => {
