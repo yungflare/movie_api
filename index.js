38d4e23d686874ac5a397143d7a71d47fe0b6252
Daniela Codes <daniela.antonio.430@gmail.com>
2024-02-11 18:29:18 -0500

    Updating Task 2.5
    Added Top movies to the Movies list
    
    Fixed express code to "res.send()" (index.js)
    
    Added more content to documentation file
    
    Added more content to stylesheet

--- index.js
+++ index.js
@@ -84,6 +84,56 @@ let movies = [
     title:'Past Lives',
     director:'Celine Song',
     genre:'Romance'
+  },
+  {
+    title:'Shrek',
+    director:'Andrew Adamson',
+    genre:'Animation'
+  },
+  {
+    title:'Fight Club',
+    director:'David Fincher',
+    genre:'Drama'
+  },
+  {
+    title:'Finding Nemo',
+    director:'Andrew Stanton',
+    genre:'Animation'
+  },
+  {
+    title:'Halloween',
+    director:'David Gordon Green',
+    genre:'Thriller'
+  },
+  {
+    title:'Half Baked',
+    director:'Tamra Davis',
+    genre:'Comedy'
+  },
+  {
+    title:'Paid in Full',
+    director:'Charles Stone III',
+    genre:'Crime'
+  },
+  {
+    title:'Boyz n the Hood',
+    director:'John Singleton',
+    genre:'Crime'
+  },
+  {
+    title:'Juice',
+    director:'Ernest Dickerson',
+    genre:'Crime'
+  },
+  {
+    title:'Fractured',
+    director:'Brad Anderson',
+    genre:'Thriller'
+  },
+  {
+    title:'A Silent Voice',
+    director:'Naoko Yamada',
+    genre:'Romance'
   }
 ];
 
@@ -122,38 +172,22 @@ app.use(methodOverride());
 
 // Get list of all movies 
 app.get('/movies', (req,res) => {
-  res.json(movies);
+  res.send('List of all movies');
 })
 
 // getting a movie by title 
 app.get('/movies/:title', (req, res) => {
-  res.json(movies.find( (movie) => {
-    return movie.title === req.params.title;
-}));
+  res.send('Data about a movie by title');
 });
 
 // getting data about a genre by name / title 
 app.get('/movies/genre/:genreName', (req, res) => {
-  const { genreName } = req.params;
-  const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
-
-  if (genre) {
-    res.status(200).json(genre);
-  }else {
-    res.status(400).send('Genre not found')
-  }
+  res.send('Data about a genre by name/title');
 })
 
 // getting data about a director by name 
 app.get('/movies/directors/:directorName', (req, res) => {
-  const { directorName } = req.params;
-  const director = movies.find(movie => movie.Director.Name === directorName).Director;
-
-  if (director) {
-    res.status(200).json(director);
-  }else {
-    res.status(400).send('Director not found')
-  }
+  res.send('Data about a director by name');
 });
 
  // registration for users 
@@ -174,7 +208,7 @@ app.post('/users/:id/favorites', (req, res) => {
 });
 
 // Deleting a movie from users favorites 
-app.delete('/users/id:/favorites/:movieId', (req, res) => {
+app.delete('/users/:id/favorites/:movieId', (req, res) => {
   const userId = req.params.id;
   const movieId = req.params.movieId;
   res.send('Confirmation that a movie has been removed from favorites');
@@ -205,25 +239,33 @@ app.listen(8080, () => {
 
 
 
+// If statements for code 
+  // res.json(movies);
+  // res.json(movies.find( (movie) => {
+ //     return movie.title === req.params.title;
+// }));
+ // const { genreName } = req.params;
+  // const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
+
+  // if (genre) {
+  //   res.status(200).json(genre);
+  // }else {
+  //   res.status(400).send('Genre not found')
+  // }
+  // const { directorName } = req.params;
+  // const director = movies.find(movie => movie.Director.Name === directorName).Director;
+
+  // if (director) {
+  //   res.status(200).json(director);
+  // }else {
+  //   res.status(400).send('Director not found')
+  // }
 
 
-// code for exercise 2.4 
 
-// app.get('/movies', (req, res, next) => {
-//    res.json(topMovies);
-// });
-  
-// app.get('/', (req, res) => {
-//   res.send('This is the endpoint! ');
-// });
 
-// app.use((err, req, res, next) => {
-//   console.error(err.stack);
-//   res.status(500).send('Something went wrong!');
-// });
 
-// app.listen(8080, () => {
-//   console.log('Your app is listening on port 8080.');
-// });
+
+
 
 
