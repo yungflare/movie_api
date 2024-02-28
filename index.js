4cfd4a9e7dfc1b21f62d1bca22b6c0fbc0db7402
Daniela Codes <daniela.antonio.430@gmail.com>
2024-02-28 17:49:18 -0500

    Updated index.js
    Updated mongoose.connect with Render URI

--- index.js
+++ index.js
@@ -24,7 +24,10 @@ const Users = Models.User;
 const Genres = Models.Genre;
 const Directors = Models.Director;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB'); //, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB'); //, { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect( process.env.CONNECTION_URI);
+
 
 let myLogger = (req,res,next) => {
   console.log(req.url);
