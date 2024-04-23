78a7d76dfb45fd4509dd3d5da9360c40bfa3543c
Daniela Codes <daniela.antonio.430@gmail.com>
2024-04-22 20:45:28 -0400

    back to what was
    no reverting here

--- index.js
+++ index.js
@@ -11,22 +11,8 @@ app.use(express.urlencoded({
 }));
 
 const cors = require('cors');
-<<<<<<< HEAD
-let allowedOrigins = ['http://localhost:8080', '`https://movie-api-kiz1.onrender.com'];
-=======
-let allowedOrigins = ['http://localhost:8080', 'https://movie-api-kiz1.onrender.com', 'http://localhost:1234'];
->>>>>>> parent of aca2a22 (ok)
+app.use(cors());
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
